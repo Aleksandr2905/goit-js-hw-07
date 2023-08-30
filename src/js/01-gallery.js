2ab77fc7720b9f8b06cca556175992b41c9083ce
Aleksandr2905 <natalukha@ukr.net>
2023-08-30 23:39:41 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+ galleryEl: document.querySelector('.gallery'),  
+}
+
+function renderGalleryItem(galleryItems) {
+    const itemsMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+ </li>`;
+    }).join(''); 
+    
+    refs.galleryEl.innerHTML = itemsMarkup;
+    
+}
+renderGalleryItem(galleryItems);
+
+const onImgClick = event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+} 
+
+refs.galleryEl.addEventListener('click', onImgClick);
+
+
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
+
