f70d3825911a3ba4694ef724af6fce28684528c8
Aleksandr2905 <natalukha@ukr.net>
2023-08-31 23:43:52 +0300

    task02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+ galleryEl: document.querySelector('.gallery'),  
+}
+
+function renderGalleryItem(galleryItems) {
+    const itemsMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+ </li>`;
+    }).join(''); 
+    
+    refs.galleryEl.innerHTML = itemsMarkup;
+    
+}
+renderGalleryItem(galleryItems);
+
+
+var gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
